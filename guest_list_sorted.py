3c7624187afd1e9cec4e3471ce9a67c6f6f59ca3
alyssa <alyssa.companioni@gmail.com>
2026-02-07 20:11:05 -0500

    Completes guest_list_sorted.py assignment.

---
+++ guest_list_sorted.py
@@ -0,0 +1,19 @@
+guests = ['Chris', 'Sam', 'Katie Lauren']
+
+# print(guests[0] + ", you are invited to dinner to meet my friends.")
+# print(guests[1] + ", you are invited to dinner to meet my boyfriend.")
+# print(guests[2] + ", you are invited to dinner to meet my boyfriend.")
+
+print("I have found a larger dinner table to host more guests!")
+
+guests.insert(0, 'Rachel')
+guests.insert(2, 'Hamish')
+guests.append('Ben')
+
+guests.sort()
+
+print(guests[0] + ", you are invited to dinner to meet my boyfriend.")
+print(guests[1] + ", you are invited to dinner to meet my friends.")
+print(guests[2] + ", you are invited to dinner to meet my boyfriend.")
+print(guests[3] + ", you are invited to dinner to meet my boyfriend.")
+print(guests[4] + ", you are invited to dinner to meet my boyfriend.")

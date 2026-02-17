334d6cc14e40d83d3c81ce1cadfbc891d0b565b5
alyssa <alyssa.companioni@gmail.com>
2026-02-17 17:20:46 -0500

    Completes animals.py assignment, 4-2.

---
+++ animals.py
@@ -0,0 +1,8 @@
+# Assignment 4-2: Animals
+
+animals = ['crayfish', 'crab', 'lobster']
+
+for animal in animals:
+  print("There once was a " + animal + " who lived near the water...")
+
+print("All of these animals have an exoskeleton.")

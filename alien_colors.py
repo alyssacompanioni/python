5ebbe8840741aa3a0c00f56c88eef1e6925f365e
alyssa <alyssa.companioni@gmail.com>
2026-02-23 23:51:24 -0500

    completes alien_colors.py

---
+++ alien_colors.py
@@ -0,0 +1,22 @@
+alien_color = "red"
+
+# if alien_color == "green":
+#   print("You just earned 5 points for shooting a green alien!")
+# elif alien_color == "yellow":
+#   print("You just earned 10 points for shooting a yellow alien!")
+# else:
+#   print("You just earned 15 points for shooting a red alien!")
+
+if alien_color == "green":
+  print("You just earned 5 points for shooting a green alien!")
+elif alien_color == "yellow":
+  print("You just earned 10 points for shooting a yellow alien!")
+elif alien_color == "red":
+  print("You just earned 15 points for shooting a red alien!")
+
+# if alien_color == "green":
+#   print("You just earned 5 points for shooting a green alien!")
+# if alien_color == "yellow":
+#   print("You just earned 10 points for shooting a yellow alien!")
+# if alien_color == "red":
+#   print("You just earned 15 points for shooting a red alien!")
